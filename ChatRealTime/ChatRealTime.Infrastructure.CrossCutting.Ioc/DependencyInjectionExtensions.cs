223a112e5eeb07653adceaf901926639e93603eb
nicholascatao@yahoo.com <redes H1X1>
2023-05-01 21:48:42 -0300

    Ajustes DI

--- ChatRealTime/ChatRealTime.Infrastructure.CrossCutting.Ioc/DependencyInjectionExtensions.cs
+++ ChatRealTime/ChatRealTime.Infrastructure.CrossCutting.Ioc/DependencyInjectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using ChatRealTime.Application.Interfaces;
+using ChatRealTime.Application.Mappers;
 using ChatRealTime.Application.Services;
 using ChatRealTime.Domain.Core.Interfaces;
 using ChatRealTime.Domain.Models;
@@ -18,21 +19,39 @@ namespace ChatRealTime.Infrastructure.CrossCutting.Ioc
             services.UseApplications();
             services.UseServices();
             services.UseRepositories();
+            services.UserMappers();
         }
 
         public static void UseApplications(this IServiceCollection services) 
         {
             services.AddScoped<IUserAppService, UserAppService>();
+            services.AddScoped<IUploadAppService, UploadAppService>();
+            services.AddScoped<IRoomAppService, RoomAppService>();
+            services.AddScoped<IMessageAppService, MessageAppService>();
+            services.AddScoped<IMessageMapper, MessageMapper>();
+            services.AddScoped<IRoomMapper, RoomMapper>();
+            services.AddScoped<IUserMapper, UserMapper>();
         }
 
         public static void UseServices(this IServiceCollection services)
         {
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IRoomService, RoomService>();
+            services.AddScoped<IMessageService, MessageService>();
         }
 
         public static void UseRepositories(this IServiceCollection services)
         {
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+        }
+
+        public static void UserMappers(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MessageMapper));
+            services.AddAutoMapper(typeof(RoomMapper));
+            services.AddAutoMapper(typeof(UserMapper));
         }
 
         public static void UseIdentity(this IServiceCollection services)
